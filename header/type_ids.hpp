08eb9cb3262d4c1eaf3a95091d997ad83c097308
Jakob Otto <jakob.otto@haw-hamburg.de>
2021-04-15 16:42:44 +0200

    Add stateful actor example

--- header/type_ids.hpp
+++ header/type_ids.hpp
@@ -1,26 +1,11 @@
-/******************************************************************************
- *                       ____    _    _____                                   *
- *                      / ___|  / \  |  ___|    C++                           *
- *                     | |     / _ \ | |_       Actor                         *
- *                     | |___ / ___ \|  _|      Framework                     *
- *                      \____/_/   \_|_|                                      *
- *                                                                            *
- * Copyright 2011-2020 Jakob Otto                                             *
- *                                                                            *
- * Distributed under the terms and conditions of the BSD 3-Clause License or  *
- * (at your option) under the terms and conditions of the Boost Software      *
- * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
- *                                                                            *
- * If you did not receive a copy of the license files, see                    *
- * http://opensource.org/licenses/BSD-3-Clause and                            *
- * http://www.boost.org/LICENSE_1_0.txt.                                      *
- ******************************************************************************/
+/**
+ * @author Jakob Otto
+ * @email jakob.otto@haw-hamburg.de
+ * @file type_ids.hpp
+ */
 
 #pragma once
 
-#include <cstdint>
-
-#include "caf/fwd.hpp"
 #include "caf/type_id.hpp"
 
 CAF_BEGIN_TYPE_ID_BLOCK(packet_routing, caf::first_custom_type_id)
@@ -28,8 +13,9 @@ CAF_BEGIN_TYPE_ID_BLOCK(packet_routing, caf::first_custom_type_id)
   // Every CUSTOM type has to be declared here! CAF messages use
   // CAF_ADD_TYPE_ID(packet_routing, (some_type))
 
-  // Atoms are trait types that are only used for identifying messages. No
-  // members whatsoever.
+  // Atoms are trait types that are only used for identifying messages.
+  CAF_ADD_ATOM(packet_routing, tell_atom)
+  CAF_ADD_ATOM(packet_routing, tell_func_atom)
   CAF_ADD_ATOM(packet_routing, hello_world_atom)
 
 CAF_END_TYPE_ID_BLOCK(packet_routing)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 /**
  * @author Jakob Otto
  * @email jakob.otto@haw-hamburg.de
- *
- * main.cpp - Part of the packet routing
+ * @file main.cpp
  */
 
 #include "caf/all.hpp"
@@ -13,30 +12,15 @@ using namespace caf;
 
 namespace {
 
-behavior print_actor(event_based_actor* self) {
-  return {
-    [=](hello_world_atom, size_t num_hellos) {
-      for (size_t count = 0; count < num_hellos; ++count)
-        aout(self) << "Hello World!" << std::endl;
-      self->quit(); // quits the actor after running the complete handler.
-    },
-  };
-}
-
 struct config : caf::actor_system_config {
   config() {
     caf::init_global_meta_objects<caf::id_block::packet_routing>();
-    opt_group{custom_options_, "global"}.add(num_hellos, "num_hellos,n",
-                                             "number of hellos to print");
+    opt_group{custom_options_, "global"};
   }
-
-  size_t num_hellos = 1;
 };
 
 void caf_main(caf::actor_system& sys, const config& args) {
-  scoped_actor self{sys};
-  auto printer = sys.spawn(print_actor);
-  self->send(printer, hello_world_atom_v, args.num_hellos);
+  // nop
 }
 
 } // namespace
