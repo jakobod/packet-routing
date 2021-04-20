4a51e5903ddba8df1de1f3ab04bb87c7d3271945
Jakob Otto <jakob.otto@haw-hamburg.de>
2021-04-20 18:31:16 +0200

    Add message abstraction

---
+++ header/routing/message.hpp
@@ -0,0 +1,53 @@
+/**
+ * @author Jakob Otto
+ * @email jakob.otto@haw-hamburg.de
+ * @file table.hpp
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "caf/actor.hpp"
+
+namespace routing {
+
+/// A message that can be routed through the network of nodes.
+struct message {
+  message(std::string content, caf::actor destination);
+
+  message() = default;
+
+  // -- members ----------------------------------------------------------------
+
+  const std::string& content();
+
+  const caf::actor& destination() const;
+
+  uint64_t path_length();
+
+  const std::vector<caf::actor>& path();
+
+  // -- public-API -------------------------------------------------------------
+
+  void update_path(caf::actor current_hop);
+
+  void update_weight(uint64_t weight);
+
+  template <class Inspector>
+  friend bool inspect(Inspector& f, routing::message& x) {
+    return f.object(x).fields(f.field("content", x.content_),
+                              f.field("destination", x.destination_),
+                              f.field("path", x.path_),
+                              f.field("path_length", x.path_length_));
+  }
+
+private:
+  std::string content_;
+  caf::actor destination_;
+  std::vector<caf::actor> path_;
+  uint64_t path_length_;
+};
+
+} // namespace routing
---
+++ src/routing/message.cpp
@@ -0,0 +1,52 @@
+/**
+ * @author Jakob Otto
+ * @email jakob.otto@haw-hamburg.de
+ * @file message.cpp
+ */
+
+#include "routing/message.hpp"
+
+#include "caf/actor_ostream.hpp"
+#include "caf/event_based_actor.hpp"
+#include "caf/fwd.hpp"
+#include "routing/message.hpp"
+#include "type_ids.hpp"
+
+using namespace caf;
+
+namespace routing {
+
+message::message(std::string content, actor destination)
+  : content_(std::move(content)), destination_(std::move(destination)) {
+  // nop
+}
+
+// -- members ----------------------------------------------------------------
+
+const std::string& message::content() {
+  return content_;
+}
+
+const caf::actor& message::destination() const {
+  return destination_;
+}
+
+uint64_t message::path_length() {
+  return path_length_;
+}
+
+const std::vector<caf::actor>& message::path() {
+  return path_;
+}
+
+// -- public-API -------------------------------------------------------------
+
+void message::update_path(caf::actor current_hop) {
+  path_.emplace_back(current_hop);
+}
+
+void message::update_weight(uint64_t weight) {
+  path_length_ += weight;
+}
+
+} // namespace routing
